6fac2d5d575fdbf5b869eb21734250ab9f72c890
Evgeniy-makdak <makdak23@mail.ru>
2026-02-07 12:27:52 +0300

    Построение таблицы заданных размеров

--- colors.js
+++ colors.js
@@ -1,2 +1,12 @@
 const rows = 3;
 const cols = 3;
+const table = document.querySelector("#field");
+
+for (let i = 0; i < rows; i++) {
+    const tr = document.createElement("tr");
+    table.appendChild(tr)[i]
+    for (let j = 0; j < cols; j++) {
+        const td = document.createElement("td");
+        tr.appendChild(td)[j];
+    }
+}
