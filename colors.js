6d815c1da4e45fa2b12bafef194593ddaebb45cb
Evgeniy-makdak <makdak23@mail.ru>
2026-02-10 14:51:16 +0300

    Сформировал табличку 3х3 со случайно закрашенными ячейками

--- colors.js
+++ colors.js
@@ -1,12 +1,18 @@
 const rows = 3;
 const cols = 3;
+const colors = ["red", "green", "blue"];
+
 const table = document.querySelector("#field");
 
-for (let i = 0; i < rows; i++) {
-    const tr = document.createElement("tr");
-    table.appendChild(tr)
-    for (let j = 0; j < cols; j++) {
-        const td = document.createElement("td");
-        tr.appendChild(td);
+function randomColors() {
+    for (let i = 0; i < rows; i++) {
+        const tr = document.createElement("tr");
+        table.appendChild(tr)
+        for (let j = 0; j < cols; j++) {
+            const td = document.createElement("td");
+            tr.appendChild(td);
+            td.classList.add(colors[(Math.floor(Math.random() * colors.length))]);
+        }
     }
 }
+randomColors()
