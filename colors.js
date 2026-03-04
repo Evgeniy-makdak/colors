dbd8b3be87d5617d8a17b4394f984c8b2e857b0e
Evgeniy-makdak <makdak23@mail.ru>
2026-03-04 14:05:33 +0300

    Решение новой задачи

--- colors.js
+++ colors.js
@@ -1,13 +1,19 @@
-function likeOrDislike(buttons) {
-  let state = "Nothing";
-  for (let button of buttons) {
-    if (button !== state) {
-        state = button;
-    } else {
-        state = "Nothing";
+function onlyDigits(str) {
+  const arrString = str.split("");
+  let count = 0;
+  for (let i = 0; i < arrString.length; i++) {
+  const inRange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    if (inRange.some((valid) => valid.toString() === arrString[i])) {
+      count++;
     }
   }
-  return state;
+  console.log(count)
+  console.log(arrString.length)
+  if (count === arrString.length) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(likeOrDislike(["Like", "Dislike", "Like", "Dislike", "Dislike", "Like"]));
+console.log(onlyDigits("1\t2"));
